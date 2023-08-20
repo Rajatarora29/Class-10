81d2cf22aa9441b6749b613ddce151ff6a185f49
Rajatarora29 <142710794+Rajatarora29@users.noreply.github.com>
2023-08-20 11:40:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,6 @@
+
+console.log("Hi! I am Kaashvi");
+console.log(" I am 12 years old");
+console.log("I study in Presidium school");
+console.log("My hobbys are to draw and dance");
+console.log("My favourite color is yellow");
